9f00b797f63f79076e8aadd62844569dc04473a0
Nick Rusin <woundculture@gmail.com>
2021-11-22 23:31:20 +0000

    added AssertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// function implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// test code
+/*
+assertEqual(`Lighthouse Labs`, `Bootcamp`);
+assertEqual(1, 1);
+assertEqual(`test`, `test`);
+assertEqual(3, 4);
+*/
