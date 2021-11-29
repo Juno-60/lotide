c577cc7008509f1d92a6dd4452145fc66f371aa3
Nick Rusin <woundculture@gmail.com>
2021-11-29 21:43:30 +0000

    refactored to run tests with mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,23 @@
 const head = require('../head.js')
-const assertEqual = require('../assertEqual.js');
+const assert = require('chai').assert;
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([6]), 6);
-assertEqual(head([]), 6);
+describe("#head", () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("returns 5 for [5]", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), "Hello");
+  });
+  it("does not return a number value when given an empty array", () => {
+    assert.notStrictEqual(head([]), 6);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,34 @@
 const tail = require('../tail.js')
-const assertEqual = require('../assertEqual.js');
+const assert = require('chai').assert;
 
+describe("#tail general functions", () => {
+  it('returns [3, 5, 7, 10] for [1, 3, 5, 7, 10]', () => {
+    assert.deepEqual(tail([1, 3, 5, 7, 10]), [3, 5, 7, 10]);
+  });
+  it('returns empty array for [1]', () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it('returns empty array for []', () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
 
-// TEST CASES FOR PROPER FUNCITON
-console.log(tail([1, 3, 5, 7, 10]));
-console.log(tail([]));
-console.log(tail([1]));
+describe("#tail output checks", () => {
+  let phrase = (["Hello", "Lighthouse", "Labs"]);
+  it('returns 2 for [`Hello`, `Lighthouse`, `Labs`].length', () => {
+    assert.deepEqual(tail(phrase).length, 2);
+  });
+  it('returns "Lighthouse" for [`Hello`, `Lighthouse`, `Labs`][0]', () => {
+    assert.deepEqual(tail(phrase)[0], "Lighthouse");
+  });
+  it('returns "Labs" for [`Hello`, `Lighthouse`, `Labs`][1]', () => {
+    assert.deepEqual(tail(phrase)[1], "Labs");
+  });
+});
 
-// TEST CASES TO CHECK OUTPUT
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-// TEST CASES TO ENSURE ORIGINAL ARRAY REMAINS INTACT
-const words = [`I`, `Need`, `Beer`];
-tail(words);
-assertEqual(words.length, 3);
-const wordTail = tail(words);
-assertEqual(wordTail[1], `Beer`);
+describe("#tail check original array is not modified", () => {
+  const phrase = ['I', 'Love', 'Beer'];
+  it("return `Beer` for ['I', 'Love', 'Beer'][2]", () => {
+    assert.strictEqual(phrase[2], "Beer");
+  });
+});
